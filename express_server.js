80ca961b839cf02b2f6ebb6a30c5bfd0d70eeaa6
DashaDasha88 <dasha.kotova.mail@gmail.com>
2021-02-15 20:45:51 +0000

    added routes

--- express_server.js
+++ express_server.js
@@ -7,9 +7,26 @@ const urlDatabase = {
   "9sn5xK": "http://www.google.com"
 };
 
-app.get("/urls.json", (req, res) => {
-  res.send(urlDatabase);
+app.get("/", (req, res) => {
+  res.send("Hello!");
 });
+
+app.get("/urls.json", (req, res) => {
+  res.json(urlDatabase);
+});
+
+app.get("/hello", (req, res) => {
+  res.send("<html><body>Hello <b>World</b></body></html>\n");
+});
+
+app.get("/set", (req, res) => {
+  const a = 1;
+  res.send(`a = ${a}`);
+ });
+ 
+ app.get("/fetch", (req, res) => {
+  res.send(`a = ${a}`);
+ });
 
 app.listen(PORT, () => {
   console.log('Example app listening on port ${PORT}!');
