378a709dcf91ada499553c459913bb0db1761c6c
DashaDasha88 <dasha.kotova.mail@gmail.com>
2021-02-16 20:10:53 +0000

    added the get and post routes for the url submission form

--- express_server.js
+++ express_server.js
@@ -1,7 +1,9 @@
 const express = require("express");
 const app = express();
 const PORT = 8080; //default port 8080
+const bodyParser = require ("body-parser");
 
+app.use(bodyParser.urlencoded({extended: true}));
 app.set("view engine", "ejs");
 
 const urlDatabase = {
@@ -9,8 +11,13 @@ const urlDatabase = {
   "9sn5xK": "http://www.google.com"
 };
 
+///FUNCTIONS///
+function generateRandomString(){
+  
+}
 
-///ROUTES///
+
+///GET ROUTES///
 app.get("/", (req, res) => {
   res.send("Hello!");
 });
@@ -36,24 +43,26 @@ app.get("/set", (req, res) => {
   const templateVars = { urls: urlDatabase };
   res.render("urls_index", templateVars);
  });
+ 
+ app.get("/urls/new", (req, res) => {
+   res.render("urls_new");
+ });
 
  app.get("/urls/:shortURL", (req, res) => {
    console.log(req.params);
    console.log(urlDatabase[req.params.shortURL]);
-  const templateVars = { 
-    shortURL: req.params.shortURL, 
-    longURL: urlDatabase[req.params.shortURL]
-  };
-  res.render("urls_show", templateVars);
-});
+   const templateVars = { 
+     shortURL: req.params.shortURL, 
+     longURL: urlDatabase[req.params.shortURL]
+    };
+    res.render("urls_show", templateVars);
+  });
 
-/*app.get("/urls/new", (req, res) => {
-  res.render("urls_new");
+///POST///
+app.post("/urls", (req, res) => {
+  console.log(req.body);  // Log the POST request body to the console
+  res.send("Ok");         // Respond with 'Ok' (we will replace this)
 });
-
-app.get("/urls/:id", (req, res) => {
-  res.render("urls_new");
-});*/
 
 
 ///LISTEN///
