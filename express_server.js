40c3752ddb29627989a65963ddf1432366546b39
DashaDasha88 <dasha.kotova.mail@gmail.com>
2021-02-18 04:42:54 +0000

    added registration

--- express_server.js
+++ express_server.js
@@ -2,16 +2,38 @@ const express = require("express");
 const app = express();
 const PORT = 8080; //default port 8080
 const bodyParser = require ("body-parser");
+const cookieParser = require("cookie-parser");
 
+app.use(cookieParser());
 app.use(bodyParser.urlencoded({extended: true}));
 app.set("view engine", "ejs");
 
+
+//OBJECTS//
+
+//Original URL Database
 const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
   "9sn5xK": "http://www.google.com"
 };
 
+//Users Object
+const userDatabase = { 
+  "userRandomID": {
+    id: "userRandomID", 
+    email: "dasha.kotova.mail@gmail.com", 
+    password: "ohai"
+  },
+ "user2RandomID": {
+    id: "user2RandomID", 
+    email: "user2@example.com", 
+    password: "dishwasher-funk"
+  }
+}
+
 ///FUNCTIONS///
+
+//Generate String of Random Characters for Tiny URL
 const generateRandomString = () => {
   let string = ' ';
   const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
@@ -23,53 +45,100 @@ const generateRandomString = () => {
 }
 
 ///GET ROUTES///
+
+//Homepage
 app.get("/", (req, res) => {
   res.send("Hello!");
 });
 
-app.get("/urls.json", (req, res) => {
-  res.json(urlDatabase);
-});
-
+//URLs Index Page
 app.get("/urls", (req, res) => {
-  const templateVars = { urls: urlDatabase };
-  console.log(urlDatabase)
+  const templateVars = {
+    urls: urlDatabase
+  };
   res.render("urls_index", templateVars);
 });
- 
+
+//Create New URL page
 app.get("/urls/new", (req, res) => {
   res.render("urls_new");
 });
 
+//Display New URL page
 app.get("/urls/:shortURL", (req, res) => {
-  console.log(req.params);
-  console.log(urlDatabase[req.params.shortURL]);
   const templateVars = { 
-    shortURL: req.params.shortURL, 
+    shortURL: req.params.shortURL,
     longURL: urlDatabase[req.params.shortURL]
   };
   res.render("urls_show", templateVars);
 });
 
-app.get('/u/:shortURL', (req, res) => {
-  res.redirect(urlDatabase[req.params.shortURL]);
+//Redirect from ShortURL to FullURL page
+app.get("/u/:shortURL", (req, res) => {
+let fullURL = urlDatabase[req.params.shortURL];
+res.redirect(fullURL);
 });
 
-///POST///
+//Registration Form
+app.get("/register", (req, res) => {
+  res.render("registration");
+});
+
+///POST ROUTES///
+
+//New Short URL Page
 app.post("/urls", (req, res) => {
   let generatedShortURL = generateRandomString();
 
-  urlDatabase[generatedShortURL] = req.body.longURL
+  urlDatabase[generatedShortURL] = req.body.longURL;
 
   res.redirect(`/urls/${generatedShortURL}`);
 });
 
+//Delete URL
 app.post("/urls/:shortURL/delete", (req, res) => {
   const urlToDelete = req.params.shortURL
   delete urlDatabase[urlToDelete];
   res.redirect('/urls');
 });
 
+//Update URL
+app.post('/urls/:shortURL/', (req, res) => {
+  const shortURL = req.params.shortURL;
+  newLongURL = req.body.newURL;
+  urlDatabase[shortURL] = newLongURL;
+  res.redirect('/urls');
+});
+
+//Login
+app.post('/login', (req, res) => {
+  const email = req.body.email
+  const password = req.body.password
+
+  let currentUser = userDatabase[email]
+  
+  if (currentUser.password === password) {
+    res.redirect("urls_index");
+  } else {
+    res.send("Wrong password");
+  }
+
+});
+
+//Register
+app.post('/register', (req, res) => {
+  let userID = generateRandomString();
+
+  let userObj = {
+    id = userID,
+    email = req.body.email,
+    password = req.body.password
+  }
+
+  res.cookie("user_id", userID);
+  res.redirect("/urls");
+
+});
 
 ///LISTEN///
 app.listen(PORT, () => {
