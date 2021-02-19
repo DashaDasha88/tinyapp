f13094db3c6cfbf06eda1e8ef6cb7f9efec2912f
DashaDasha88 <dasha.kotova.mail@gmail.com>
2021-02-19 19:33:36 +0000

    helper functions refactored

--- helpers.js
+++ helpers.js
@@ -14,23 +14,30 @@ const generateRandomString = () => {
 
 //Identifies URLs specific to a user's ID
 const specificURLS = function(id, urlDatabase) {
-  const userUrls = {};
-  for (const shortURL in urlDatabase) {
+
+  let userUrls = {};
+
+  for (let shortURL in urlDatabase) {
     if (urlDatabase[shortURL].userID === id) {
       userUrls[shortURL] = urlDatabase[shortURL];
     }
   }
+
   return userUrls;
+
 };
 
 //Checks if the user e-mail already exists in the database and corresponds to a user
 const getUserByEmail = function(email, userDatabase) {
+
   for (let user in userDatabase) {
     if (userDatabase[user].email === email) {
       return true;
     }
+  } else {
+    return false;
   }
-  return false;
+  
 };
 
 module.exports = { generateRandomString, getUserByEmail, specificURLS };
